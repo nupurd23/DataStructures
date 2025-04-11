fae5a26cbf920a8f52d8f2858eb8579882d221ca
nupur_d23 <nupurdang1888@gmail.com>
2025-04-11 08:39:58 +0530

    patterns

---
+++ patterns/pattern2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i =0;i<6;i++){
+        for(int j=1;j<i+1;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
