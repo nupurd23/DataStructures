915ae09caecb2d177778cd13a74a02eb71d72f5f
nupur_d23 <nupurdang1888@gmail.com>
2025-04-18 10:10:54 +0530

    delete last node

---
+++ linked list/delete_last_node.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+Node *dellast(Node *head){
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->next == NULL){
+        delete head;
+        return NULL;
+    }
+   
+        Node *curr = head;
+        while(curr->next->next != NULL){
+            curr= curr->next;
+        }
+        delete(curr->next);
+        curr->next = NULL;
+        
+    return head;
+}
+int main(){
+   Node *head = new Node(10);
+   head->next = new Node(20);
+   head->next->next = new Node(30);
+   dellast(head);
+   cout<<(head->data)<<" ";
+   cout<<(head->next->data)<<" ";
+   cout<<(head->next->next->data);
+}
