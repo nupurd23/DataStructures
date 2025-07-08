deb80e693d4aaa2957ed6838c68ef14cacd7ba80
Nupur <nupurdang1888@gmail.com>
2025-07-08 11:31:44 +0530

    reverse queue

---
+++ queue/reverse_queue.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+void reverse(queue<int>&q){
+    stack<int>s;
+    while(q.empty()==false){
+       s.push(q.front());
+       q.pop();
+    }
+    while(s.empty()==false){
+        q.push(s.top());
+        s.pop();
+    }
+}
+int main() {
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    reverse(q);
+
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
