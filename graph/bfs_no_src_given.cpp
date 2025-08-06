a3d6cc21286842e3ec9b747ab7d7ee240cc00697
Nupur <nupurdang1888@gmail.com>
2025-08-06 09:38:24 +0530

    second version

---
+++ graph/bfs_no_src_given.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+void BFS(vector<int>adj[],int v,int s,bool visited[]){
+    queue<int>q;
+visited[s]=true;
+q.push(s);
+while(q.empty()==false){
+    int u=q.front();
+    q.pop();
+    cout<<u<<" ";
+    for(int v : adj[u]){
+        if(visited[v]==false){
+            visited[v]=true;
+            q.push(v);
+        }
+    }
+}
+}
+
+void BFSDis(vector<int>adj[],int v){
+    bool visited[v+1];
+    for(int i=0;i<v;i++){
+        visited[i]=false;
+    }
+    for(int i=0;i<v;i++){
+        if(visited[i]==false){
+            BFS(adj,v,i,visited);
+        }
+    }
+}
+
+int main() {
+    int v = 5;
+    vector<int> adj[v];
+
+    adj[0].push_back(1);
+    adj[1].push_back(0);
+    
+    adj[2].push_back(3);
+    adj[3].push_back(2);
+
+    BFSDis(adj, v);
+    return 0;
+}
